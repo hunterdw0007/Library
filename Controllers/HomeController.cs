1e2586635e26cde494d2e1d097f816cdc50f9451
Hunter Westerlund <hunterw.mail@gmail.com>
2021-11-11 18:07:01 -0600

    Have the book form made but it doesn't work

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,59 +20,69 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        //Testing Write/Read from Json
-        /*
-        Book Book = new Book {
-            vcTitle = "Bible",
-            vcAuthor = "God",
-            vcISBN = "00000000000"
-        };
-        var Books = new List<Book>();
-        Books.Add(Book);
-        WriteJson(Books);
-        */
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-
-    public List<Book> ReadJson()
-    {
-
-        string BooksJson = System.IO.File.ReadAllText(filepath);
-        List<Book> Books = JsonSerializer.Deserialize<List<Book>>(BooksJson);
-        foreach (var Book in Books)
+        var vm = new CatalogViewModel()
         {
-            Console.WriteLine(Book.ToString());
-        }
-        return Books;
+            Books = ReadJson()
+        };
+        return View(vm);
     }
 
-    public void WriteJson(List<Book> NewBooks)
+    public IActionResult BookForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult AddBook(Book book)
     {
         try
         {
+            WriteJson(book);
+            return Json(new {success = true, title = "Success", message = "Book Successfully Added!"});
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }
+    }
 
+    /*
+        The next two methods take care of reading and writing to the Json that stores the book catalog.
+        It's pretty scuffed but it works for our purposes.
+    */
+    public List<Book> ReadJson()
+    {
+        try
+        {
+            string BooksJson = System.IO.File.ReadAllText(filepath);
+            List<Book> Books = JsonSerializer.Deserialize<List<Book>>(BooksJson);
+
+            //Writes them to console for testing purposes - does not effect the site working
+            foreach (var Book in Books)
+            {
+                Console.WriteLine(Book.ToString());
+            }
+            return Books;
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }
+    }
+
+    public void WriteJson(Book NewBook)
+    {
+        try
+        {
             //Getting the inital catalog to add the new books to it
             IEnumerable<Book> catalog = ReadJson();
 
             //Combine the new books with the existing ones but exclude duplicates
-            IEnumerable<Book> Books = catalog.Union(NewBooks);
+            IEnumerable<Book> Books = catalog.Append(NewBook);
 
-            if (Books != null)
-            {
-                string JsonCatalog = JsonSerializer.Serialize(Books);
-                System.IO.File.WriteAllText(filepath, JsonCatalog);
-            }
+            string JsonCatalog = JsonSerializer.Serialize(Books);
+            System.IO.File.WriteAllText(filepath, JsonCatalog);
+
         }
         catch (Exception e)
         {
